5b42f843b361318eec9c642d8e1128e3c94eaa4e
wafer-bw <ben.jz.wakeford@gmail.com>
2020-10-22 15:11:46 -0400

    Writes default text to created firedoc

--- gapi/gapi.go
+++ gapi/gapi.go
@@ -7,6 +7,7 @@ import (
 	"io"
 	"io/ioutil"
 	"net/http"
+	"strings"
 	"time"
 
 	"golang.org/x/oauth2"
@@ -88,12 +89,12 @@ func AssignParentFolder(client *http.Client, documentID string, fireDocFolderID 
 
 // WriteDoc writes initial content to the fire doc
 func WriteDoc(client *http.Client, documentID string) error {
-	zone, offset := time.Now().Zone()
-	//location := map[string]interface{}{"segmentId": "", "index": 1}
-	endLocation := map[string]interface{}{"segmentId": ""}
-	fireDocContent := map[string]interface{}{"text": fmt.Sprintf("Timeline %s %v", zone, offset), "endOfSegmentLocation": endLocation}
-	insertText := map[string]interface{}{"insertText": fireDocContent}
-	requests := map[string][]interface{}{"requests": {insertText}}
+	lines, textRequest := fireDocTextToInsert()
+	requests := map[string][]interface{}{"requests": {
+		textRequest,
+		fireDocStyleToApply(1, len(lines[0])+1),
+		fireDocBulletsToApply(len(lines[0])+2, len(lines[1])),
+	}}
 	requestBody, err := json.Marshal(requests)
 	if err != nil {
 		return err
@@ -114,4 +115,33 @@ func WriteDoc(client *http.Client, documentID string) error {
 		return fmt.Errorf("Error writing to document %s:\n%s", documentID, body)
 	}
 	return nil
+}
+
+func fireDocStyleToApply(start int, end int) map[string]interface{} {
+	textStyle := map[string]bool{"bold": true}
+	styleRange := map[string]interface{}{"segmentId": "", "startIndex": start, "endIndex": end}
+	updateTextStyle := map[string]interface{}{"textStyle": textStyle, "fields": "*", "range": styleRange}
+	request := map[string]interface{}{"updateTextStyle": updateTextStyle}
+	return request
+}
+
+func fireDocBulletsToApply(start int, end int) map[string]interface{} {
+	bulletPreset := "BULLET_DISC_CIRCLE_SQUARE"
+	bulletRange := map[string]interface{}{"segmentId": "", "startIndex": start, "endIndex": end}
+	createParagraphBullets := map[string]interface{}{"range": bulletRange, "bulletPreset": bulletPreset}
+	request := map[string]interface{}{"createParagraphBullets": createParagraphBullets}
+	return request
+}
+
+func fireDocTextToInsert() ([]string, map[string]interface{}) {
+	now := time.Now()
+	zone, _ := now.Zone()
+	endOfSegmentLocation := map[string]interface{}{"segmentId": ""}
+	lines := []string{
+		fmt.Sprintf("Timeline (%s)", zone),
+		fmt.Sprintf("%s - Fire was called", now.Format("2006-01-02 15:04")),
+	}
+	insertText := map[string]interface{}{"text": strings.Join(lines, "\n"), "endOfSegmentLocation": endOfSegmentLocation}
+	request := map[string]interface{}{"insertText": insertText}
+	return lines, request
 }
